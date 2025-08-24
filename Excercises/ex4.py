14e5cc7fc95e40f7cee98eb502580845aa77eb71
Sneha Sahu <snehasahu730@gmail.com>
2025-08-25 01:57:21 +0530

    Add files via upload

---
+++ Excercises/ex4.py
@@ -0,0 +1,107 @@
+# Slicing and Logical concepts
+
+# 1 
+a = 'Practice Questions of List in Python'
+print(a)
+
+# 2
+a = 'Practice Questions of List in Python'
+print(len(a))
+
+# 3
+a = 'Practice Questions of List in Python'
+print(a[1:4])
+
+# 4
+a = 'Practice Questions of List in Python'
+print(a[1:7])
+
+# 5
+a = 'Practice Questions of List in Python'
+print(a[3:])
+
+# 6
+a = 'Practice Questions of List in Python'
+print(a[:5])
+
+# 7
+a = 'Practice Questions of List in Python'
+print(a[4:17])
+
+# 8
+a = 'Practice Questions of List in Python'
+print(a[-2:-5:-1])
+
+# 9
+a = 'Practice Questions of List in Python'
+print(a[1:7:1])
+
+# 10
+a = 'Practice Questions of List in Python'
+print(a[1:7:2])
+
+# 11
+a = 'Practice Questions of List in Python'
+print(a[-5:])
+
+# 12
+a = 'Practice Questions of List in Python'
+print(a[:4])
+
+# 13
+a = 'Practice Questions of List in Python'
+print(a[-2:-5:-2])
+
+# 14
+a = 'Practice Questions of List in Python'
+print(a[11:15])
+
+# 15
+a = 'Practice Questions of List in Python'
+print(a[:])
+
+# 16
+a = 'Practice Questions of List in Python'
+print(a[::2])
+
+# 17
+a = 'Practice Questions of List in Python'
+print(a[-5:-1])
+
+# 18
+a = 'Practice Questions of List in Python'
+print(a[3:-3])
+
+# 19
+a = 'Practice Questions of List in Python'
+print(a[7:1:-1])
+
+# 20
+a = 'Practice Questions of List in Python'
+print(a[30:40])
+
+# 21
+a = 'Practice Questions of List in Python'
+print(a[17:-1])
+
+# 22
+a = 'Practice Questions of List in Python'
+print(a[10::-1])
+
+# 23
+a = 'Practice Questions of List in Python'
+print(a[::-1])
+
+# 24
+a = 'Practice Questions of List in Python'
+print(a[::3])
+
+# 25
+a = 'Practice Questions of List in Python'
+print(a[-10::])
+
+
+
+
+
+
